a3a6ed1036fa2f2795b708c35908b9b3cc8e0429
HeffelsIvo <71328847+HeffelsIvo@users.noreply.github.com>
2020-09-15 16:05:33 +0200

    upload index

---
+++ js/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    
+    createCanvas(200,200);
+    background(100);
+    
+    print("Dit is de setup");
+    
+    rectMode(CENTER);
+    ellipse(100,100,200);
+    
+    rect(100,100,20);
+    
+    textAlign(CENTER,CENTER);
+    text("Dit is het midden",100,100)
+    
+}
+
+function draw() {
+    
+    
+    
+}
